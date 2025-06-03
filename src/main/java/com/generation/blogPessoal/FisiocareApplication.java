152f35d45424a59ff0d2a35089744556b87c4cbe
Aylla Scaglia <aylla@scaglia.com.br>
2025-06-03 10:50:09 -0300

    :tada: Commit Inicial

---
+++ src/main/java/com/generation/blogPessoal/FisiocareApplication.java
@@ -0,0 +1,13 @@
+package com.generation.blogPessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FisiocareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FisiocareApplication.class, args);
+	}
+
+}
